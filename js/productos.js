d321ce8f5e244c62bc44ef9e73bc375672b00c68
Juan Paez <jupaez@gmail.com>
2023-12-01 15:34:03 -0300

    Acceso a BD-sistema CRUD-Modif front cursos.html

---
+++ js/productos.js
@@ -0,0 +1,43 @@
+const { createApp } = Vue
+
+createApp({
+    data() {
+        return {
+            url: "https://jualpa.pythonanywhere.com/productos",
+            productos: [],
+            error: false,
+            cargando: true
+        }
+    },
+    created() {
+        this.fetchData(this.url)
+    },
+    methods: {
+        fetchData(url) {
+            fetch(url)
+                .then(response => response.json())
+                .then(data => {
+                    this.productos = data;
+                    this.cargando = false
+                })
+                .catch(err => {
+                    console.error(err);
+                    this.error = true
+                })
+        },
+        eliminar(producto) {
+            const url = 'https://jualpa.pythonanywhere.com/productos/' + producto;
+            var options = {
+                method: 'DELETE',
+            }
+            fetch(url, options)
+                .then(res => res.text()) // or res.json()
+                .then(res => {
+                    location.reload();
+                })
+        }
+
+    },
+
+
+}).mount('#app')
